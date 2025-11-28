d5e6e82d5093fa2fec70529529993e8bfdaa650e
Shaiju Raju <shaijur2014@gmail.com>
2025-11-28 23:11:20 +0530

    Add database integration and render book reviews in EJS

--- index.js
+++ index.js
@@ -5,13 +5,49 @@ import env from "dotenv";
 
 const app = express();
 const port = 3000;
+env.config();
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-    res.render("index.ejs");
-})
+//Data Base Configuration
+const db = new pg.Client({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+});
+db.connect();
+
+
+//Function for Fetching data from database
+async function book_review() {
+    try {
+        const result = await db.query("SELECT * FROM book_review")
+        const books = result.rows
+        return books
+    } catch (err) {
+        console.error("Error in Fetching data",err.message);
+    }
+};
+
+
+//initial route
+app.get("/", async (req, res) => {
+    const books = await book_review();
+    // console.log(books)
+    res.render("index.ejs",{book_review: books});
+});
+
+
+app.get("/new-review", (req, res) => {
+    res.render("form.ejs");
+});
+
+
+
+
 
 
 app.listen(port, ()=> {
